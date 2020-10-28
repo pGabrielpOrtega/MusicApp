2f001346edfb995ff238e31ea8ac23c8cdc5ae5c
Omaly Breve <omalybreve@yahoo.es>
2020-10-28 14:43:39 -0600

    Configurando orientacion de pantalla

---
+++ app/src/main/java/com/example/musicapp/pantalla_2.kt
@@ -0,0 +1,13 @@
+package com.example.musicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pantalla_2.*
+
+class pantalla_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_2)
+
+        }
+}
