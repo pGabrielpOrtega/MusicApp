60aa3708e4836f5e40a5cd4b1dda6d77685f7241
Omaly Breve <omalybreve@yahoo.es>
2020-10-30 21:12:42 -0600

    Anadiendo mas pantallas y perfeccionando el diseno

---
+++ app/src/main/java/com/example/musicapp/pantalla_canciones.kt
@@ -0,0 +1,11 @@
+package com.example.musicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalla_canciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_canciones)
+    }
+}
