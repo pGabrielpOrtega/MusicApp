0e2302ba041f60102c12873596ad6f5277c75eb7
gabri <gabriel_arturo@yahoo.com>
2020-11-04 18:28:37 -0600

    chingado

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -5,17 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_pantalla_2.*
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnIr.setOnClickListener {
-            val intent:Intent = Intent(this,pantalla_2::class.java)
-            startActivity(intent)
-        }
 
+        mp = MediaPlayer.create(this, R.raw.music)
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+        totalTime = mp.duration
 
     }
 
+
+
+    fun playMusica(view: View) {
+        if (mp.isPlaying) {
+            // Stop
+            mp.pause()
+            img_play.setBackgroundResource(R.drawable.boton_play)
+
+        } else {
+            // Start
+            mp.start()
+            img_play.setBackgroundResource(R.drawable.stop)
+
+
+
+        }
+
+    }
 }
